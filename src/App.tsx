99f8d83be11aa6cc6c9a8e6a3ab7cbf8f0fe7ec9
Nick Selivanov <edu.selivanov@yandex.ru>
2024-10-16 18:06:51 +0300

    получение данных от сервера

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,14 @@
+import { useEffect } from 'react';
 import './App.css';
-
+const api = 'https://api.skilla.ru/mango/getList'
 function App() {
+useEffect(()=>{
+  fetch(api, {
+    method: 'POST',
+    headers: { Authorization: 'Bearer testtoken'}
+  }).then(response => response.json()).then(console.log).catch(console.error)
+},[])
+
   return (
     <div className="App">
      
